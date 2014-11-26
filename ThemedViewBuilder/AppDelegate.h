173d634329d65c5cef2c0a27b57d1099a46cbee0
Alex Antonyuk <alex@antonyuk.me>
2014-11-27 00:30:38 +0200

    Initial Commit

---
+++ ThemedViewBuilder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThemedViewBuilder
+//
+//  Created by Alex Antonyuk on 11/27/14.
+//  Copyright (c) 2014 Alex Antonyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThemedViewBuilder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThemedViewBuilder
+//
+//  Created by Alex Antonyuk on 11/27/14.
+//  Copyright (c) 2014 Alex Antonyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
